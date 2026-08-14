fd237c88d9c964fbd02cf4a3431fa01f7cdfb289
murilodcosta <murilodiasdacosta@gmail.com>
2026-08-14 15:46:16 -0300

    feat: implement UrlMappingRepository with derived queries

---
+++ src/main/java/dev/murilodcosta/url_shortener/repository/UrlMappingRepository.java
@@ -0,0 +1,15 @@
+package dev.murilodcosta.url_shortener.repository;
+
+import dev.murilodcosta.url_shortener.model.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlMappingRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByShortCode(String shortCode);
+
+    boolean existsByShortCode(String shortCode);
+}
