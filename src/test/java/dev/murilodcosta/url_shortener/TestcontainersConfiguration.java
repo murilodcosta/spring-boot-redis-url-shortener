4e081355b45a62dd5c2c254965d6068d29daa693
murilodcosta <murilodiasdacosta@gmail.com>
2026-09-01 12:52:02 -0300

    fix: make TestcontainersConfiguration public for sub-packages

--- src/test/java/dev/murilodcosta/url_shortener/TestcontainersConfiguration.java
+++ src/test/java/dev/murilodcosta/url_shortener/TestcontainersConfiguration.java
@@ -8,17 +8,17 @@ import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
+	public PostgreSQLContainer postgresContainer() {
 		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
 	}
 
 	@Bean
 	@ServiceConnection(name = "redis")
-	GenericContainer<?> redisContainer() {
+	public GenericContainer<?> redisContainer() {
 		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
 	}
 
